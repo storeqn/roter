69fb6117279433a165f9af036810e760cd097c09
storeqn <kf324p@gmail.com>
2026-09-04 00:15:04 +0300

    Bump cache after return button fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yasri-pwa-v14';
+const CACHE='yasri-pwa-v15';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
