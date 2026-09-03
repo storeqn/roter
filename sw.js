c9e2edfe7d667b07cf0c0349ef1ef3d33f574590
storeqn <kf324p@gmail.com>
2026-09-03 23:11:54 +0300

    Bump cache for redesigned driver page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yasri-pwa-v13';
+const CACHE='yasri-pwa-v14';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
