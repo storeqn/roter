2a1cacc93b62aafe0d6c147a4d89213733293d77
storeqn <kf324p@gmail.com>
2026-09-03 22:29:53 +0300

    Bump PWA cache after driver order isolation fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yasri-pwa-v12';
+const CACHE='yasri-pwa-v13';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
