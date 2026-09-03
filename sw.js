0d40b79cb595f31c5b6e86a7d6c9e09f3b721fc0
storeqn <kf324p@gmail.com>
2026-09-03 15:02:24 +0300

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='yasri-pwa-v9';
+self.addEventListener('install',event=>{self.skipWaiting();});
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+});
